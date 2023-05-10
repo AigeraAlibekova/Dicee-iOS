33c28eea5b6629341522234c87b3bc27d86e2dab
Aigera <aikoaliask@gmail.com>
2023-05-10 21:00:47 +0600

    first commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Who.What = Value
+      /*  diceImageView1.image = UIImage(named: "DiceThree")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = UIImage(named: "DiceTwo")*/
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+      /* diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][rightDiceNumber]
+        
+       leftDiceNumber += 1
+        rightDiceNumber -= 1*/
+    
+       /* diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][Int.random(in: 0...5)]*/
+        
+        let diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)] // diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
+    
+//    naming Conventions: camelCase - in Swift - diceImageView; kebab-case; snake_case
+//    command+ / = //
+    /* multiline comment*/
+//    print("Hello \(2+3) World") - string interpolation
+    
+//    array.shuffle()
 }
 
